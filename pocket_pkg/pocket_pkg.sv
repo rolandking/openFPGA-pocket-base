2f0f84f5e15fe3208fe97443c3e6e4e5f44715c9
Roland King <rols@rols.org>
2023-12-26 08:13:33 +0800

    added interact menu

--- pocket_pkg/pocket_pkg.sv
+++ pocket_pkg/pocket_pkg.sv
@@ -11,4 +11,51 @@ package pocket_pkg;
         logic [7:0] blue;
     } rgb_t;
 
+    typedef enum logic [3:0] {
+        controller_none               = 4'd0,
+        controller_builtin            = 4'd1,
+        controller_docked_no_analogue = 4'd2,
+        controller_docked_analogue    = 4'd3,
+        controller_docked_keyboard    = 4'd4,
+        controller_docked_mouse       = 4'd5
+    } controller_type_t;
+
+    /*
+     *              (X)
+     *          (Y)     (A)
+     *              (B)
+    */
+    typedef struct packed {
+        controller_type_t   controller_type;
+        logic [11:0]        _unused;
+        logic               face_start;
+        logic               face_select;
+        logic               trig_r3;
+        logic               trig_l3;
+        logic               trig_r2;
+        logic               trig_l2;
+        logic               trig_r1;
+        logic               trig_l1;
+        logic               face_y;
+        logic               face_x;
+        logic               face_b;
+        logic               face_a;
+        logic               dpad_right;
+        logic               dpad_left;
+        logic               dpad_down;
+        logic               dpad_up;
+    } key_t;
+
+    typedef struct packed {
+        logic [7:0] rstick_y;
+        logic [7:0] rstick_x;
+        logic [7:0] lstick_y;
+        logic [7:0] lstick_x;
+    } joy_t;
+
+    typedef struct packed {
+        logic [7:0] rtrig;
+        logic [7:0] ltrip;
+    } trig_t;
+
  endpackage
