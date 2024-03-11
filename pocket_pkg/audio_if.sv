59fc68ced26279447da1b610a1bab3f8a99d451a
Roland King <rols@rols.org>
2024-03-12 00:03:09 +0800

    added a standard audio module

--- pocket_pkg/audio_if.sv
+++ pocket_pkg/audio_if.sv
@@ -54,3 +54,46 @@ module audio_dummy(
     end
 
 endmodule
+
+    module audio_standard(
+        input logic              clk_12_288_mhz,
+        input logic signed[15:0] sound_l,
+        input logic signed[15:0] sound_r,
+
+        audio_if                 audio
+    );
+
+    // clock is 12.288MHz
+    // = 48000 * 32 * 2 * 4 == 48000 * 256
+    // this is mclk, 256 cycles of mclk for one L/R pair
+    // every 4 cycles of that we shift the audio data,
+    // every 32 cycles of that (128) we flip l/r and every 2 (256)
+    // we take new audio data
+
+    // count 0-256 for one complete cycle,
+    // bit 7 is L/R
+    logic  [7:0] counter;
+    logic [63:0] shifter;
+
+    logic signed [16:0] sound_total;
+    always_comb sound_total = sound_l + sound_r;
+
+    always @(posedge clk_12_288_mhz) begin
+        counter <= counter + 8'd1;
+
+        if( counter[1:0] == '1) begin
+            shifter <= {shifter[62:0], shifter[63]};
+        end
+
+        if(counter[7:0] == '1) begin
+            shifter    <= {1'b0, sound_total[16:1], 15'b0, 1'b0, sound_total[16:1], 15'b0};
+        end
+    end
+
+    always_comb begin
+        audio.lrck = counter[7];
+        audio.mclk = clk_12_288_mhz;
+        audio.dac  = shifter[63];
+    end
+
+endmodule
